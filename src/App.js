d24120e92571a159312b16543b69dcf19444be0a
Nceku Moyo <nhmoyo7@gmail.com>
2023-04-12 20:04:38 +0200

    fetch announcements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [anns, setAnns] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect ran!");
+    fetch("http://localhost:8000/api/v1/ann/", {
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setAnns(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>Hello, World!</main>
+      <div className="container">
+        <div className="row">
+          {anns?.map((a) => (
+            <div className="col-md-3 mb-3" key={a.id}>
+              <div className="card bg-dark text-light">
+                <div className="card-header p-2">
+                  <span className="fw-bold text-info">{a.entity_name}</span>
+                  <br />
+                  <span className="fw-bold text-warning">{a.department_name}</span>
+                </div>
+                <div className="card-body p-2">
+                  <h2 className="card-title text-info">{a.title}</h2>
+                  <span className="fw-bold">{a.start}</span> -<span className="fw-bold">{a.end}</span>
+                  <br />
+                  <span className="fw-bold">{a.venue}</span>
+                  <p className="">{a.info}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
